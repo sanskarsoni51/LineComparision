71e3bad311c9d0ac7ed38565b8dfa157f57e129b
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-15 23:19:44 +0530

    first commit

---
+++ Compare.java
@@ -0,0 +1,7 @@
+package Problems.LineComparision;
+
+public class Compare {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparision program");
+    }
+}
